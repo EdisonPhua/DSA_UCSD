54b432f93e85e3d87d625cb5c8e4ff3cda222bf7
EdisonPhua <edison.phua2004@gmail.com>
2023-05-12 11:53:04 +0000

    Fri, May 12, 2023, 7:53 PM +08:00

--- Algorithmic ToolBox/Week1/tester.py
+++ Algorithmic ToolBox/Week1/tester.py
@@ -1,5 +1,8 @@
 def max_pairwise_product(numbers):
-    first = max(numbers)
+    sort = sorted(numbers, reverse=False)
+    product = sort[0] * sort[1]
+    return product
+    
 
 if __name__ == '__main__':
     _ = int(input())
