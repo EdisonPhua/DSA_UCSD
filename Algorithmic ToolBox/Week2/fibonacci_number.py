6b824a0679c481fc70f29b2f2cfb2e89bdc50d0c
EdisonPhua <edison.phua2004@gmail.com>
2023-05-13 14:38:38 +0000

    Sat, May 13, 2023, 10:38 PM +08:00

--- Algorithmic ToolBox/Week2/fibonacci_number.py
+++ Algorithmic ToolBox/Week2/fibonacci_number.py
@@ -1,8 +1,18 @@
 import time
 
-
-
 def fibonacci_number(n):
+    if n <= 1:
+        return n
+    else:
+        a,b = 0,1
+        for _ in range(2, n+2):
+            a,b = b, (b+a)
+        return b
+            
+
+
+
+def fibonacci_number1(n):
     if n <= 1:
         return n
 
