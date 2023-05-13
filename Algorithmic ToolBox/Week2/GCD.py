4d741b2e6e16e2982dd157ee48b76c834e26a823
EdisonPhua <edison.phua2004@gmail.com>
2023-05-13 15:19:39 +0000

    Sat, May 13, 2023, 11:19 PM +08:00

--- Algorithmic ToolBox/Week2/GCD.py
+++ Algorithmic ToolBox/Week2/GCD.py
@@ -1,9 +1,13 @@
 import time
 def gcd(a, b):
+    list = [a,b]
+    list.sort()
     while b:
-        a, b = b, a % b
+        list[1], b = b, a % b
     return a
 
 if __name__ == "__main__":
     a, b = map(int, input().split())
     print(gcd(a, b))
+    
+    
