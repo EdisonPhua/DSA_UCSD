94949d2f1f6e98cc3a84a0a21001bf324f2caae8
EdisonPhua <edison.phua2004@gmail.com>
2023-05-13 08:55:14 +0000

    Sat, May 13, 2023, 4:55 PM +08:00

--- Algorithmic ToolBox/Week2/lcm.py
+++ Algorithmic ToolBox/Week2/lcm.py
@@ -1,4 +1,7 @@
+import time
 def lcm(a,b):
+    start_time = time.time()
+    
     mult = 1
     list = [a,b]
     list.sort()
@@ -11,17 +14,22 @@ def lcm(a,b):
             mult += 1
             continue
         
+    end_time = time.time()
+    return ( end_time -start_time )
         
 def lcm1(a, b):
+    start_time = time.time()
     for l in range(1, a * b + 1):
         if l % a == 0 and l % b == 0:
             return l
 
     assert False
-            
+    end_time = time.time()
+    return ( end_time -start_time )
         
 
 
 if __name__ == '__main__':
     a, b = map(int, input().split())
     print(lcm(a, b))
+    print(lcm1(a, b))
