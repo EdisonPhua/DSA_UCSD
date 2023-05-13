636d5dad76cd65b6975d8fb439024e27819495ea
EdisonPhua <edison.phua2004@gmail.com>
2023-05-13 14:50:17 +0000

    Sat, May 13, 2023, 10:50 PM +08:00

--- Algorithmic ToolBox/Week2/fibonacci_number.py
+++ Algorithmic ToolBox/Week2/fibonacci_number.py
@@ -1,4 +1,4 @@
-import time
+
 
 def fibonacci_number(n):
     if n <= 1:
@@ -8,27 +8,8 @@ def fibonacci_number(n):
         for _ in range(2, n+1):
             a,b = b, (b+a)
         return b
-            
-
-
-
-def fibonacci_number1(n):
-    if n <= 1:
-        return n
-
-    return fibonacci_number(n - 1) + fibonacci_number(n - 2)
 
 
 if __name__ == '__main__':
     input_n = int(input())
-    
-    
-    start = time.time()
     print(fibonacci_number(input_n))
-    end = time.time()
-    print((end-start)*1000)
-    
-    start = time.time()
-    print(fibonacci_number1(input_n))
-    end = time.time()
-    print((end-start)*1000)
