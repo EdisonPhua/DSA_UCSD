1073e57db3ae1123c8826b6af892ad6d67fe690e
EdisonPhua <edison.phua2004@gmail.com>
2023-05-13 22:59:53 +0800

    Revert "Revert "Sat, May 13, 2023, 10:55 PM +08:00""
    This reverts commit 4b169a927385804f277395ce7485423ad36103c4.

--- Algorithmic ToolBox/Week2/fibonacci_last_digit.py
+++ Algorithmic ToolBox/Week2/fibonacci_last_digit.py
@@ -1,16 +1,14 @@
-def fibonacci_last_digit(n):
+
+def fibonacci_number(n):
     if n <= 1:
         return n
-    previous=0
-    current=1
-    for _ in range(n-1):
-        previous, current = current, current+ previous
-    return current % 10
-        
-        
-    
+    else:
+        a,b = 0,1
+        for _ in range(2, n+1):
+            a,b = b, (b+a)
+        return b
 
 
 if __name__ == '__main__':
-    n = int(input())
-    print(fibonacci_last_digit(n))
+    input_n = int(input())
+    print(fibonacci_number(input_n))
