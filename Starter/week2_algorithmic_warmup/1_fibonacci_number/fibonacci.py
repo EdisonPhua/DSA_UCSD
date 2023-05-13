1c55f9b352b630b6eed3ef1761b0ad9e2990e0b3
EdisonPhua <edison.phua2004@gmail.com>
2023-05-13 15:10:15 +0000

    Sat, May 13, 2023, 11:10 PM +08:00

--- Starter/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ Starter/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -1,10 +1,31 @@
+import time
 def fibonacci_number(n):
     if n <= 1:
         return n
-
-    return fibonacci_number(n - 1) + fibonacci_number(n - 2)
+    else:
+        a,b = 0,1
+        for i in range(2, n+1):
+            a,b = b%10, (b+a)%10
+        return b%10
+    
+def calc_fib_fast(n):
+   f = []
+   f.append(0)
+   f.append(1)
+   for i in range(2,n+1):
+       f.append((f[i-1]%10+f[i-2]%10))
+   return f[-1]%10
 
 
 if __name__ == '__main__':
     input_n = int(input())
+    
+    start = time.time()
     print(fibonacci_number(input_n))
+    end = time.time()
+    print((end - start)*1000)
+
+    start = time.time()
+    print(calc_fib_fast(input_n))
+    end = time.time()
+    print((end - start)*1000)
