bea9d60b02430356a457f3a4d64af71ad4b46034
EdisonPhua <edison.phua2004@gmail.com>
2023-05-13 10:01:45 +0000

    Sat, May 13, 2023, 6:01 PM +08:00

--- Algorithmic ToolBox/Week2/fibonacci_sum_last_digit.py
+++ Algorithmic ToolBox/Week2/fibonacci_sum_last_digit.py
@@ -1,16 +1,17 @@
-def fibonacci_huge_naive(n, m):
+def fibonacci_sum(n):
     if n <= 1:
         return n
 
-    previous = 0
-    current  = 1
+    previous, current, _sum = 0, 1, 1
 
     for _ in range(n - 1):
         previous, current = current, previous + current
+        _sum += current
 
-    return current % m
+    return _sum % 10
 
 
 if __name__ == '__main__':
-    n, m = map(int, input().split())
-    print(fibonacci_huge_naive(n, m))
+    n = int(input())
+    print(fibonacci_sum(n))
+
