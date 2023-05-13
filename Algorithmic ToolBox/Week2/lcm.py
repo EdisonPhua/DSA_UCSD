b4e9bedf472333d7fd3f8982a9baab379c9f713f
EdisonPhua <edison.phua2004@gmail.com>
2023-05-13 09:16:05 +0000

    Sat, May 13, 2023, 5:16 PM +08:00

--- Algorithmic ToolBox/Week2/lcm.py
+++ Algorithmic ToolBox/Week2/lcm.py
@@ -1,6 +1,4 @@
-import time
 def lcm(a,b):
-    start_time = time.time() 
     mult = 1
     list = [a,b]
     list.sort()
@@ -11,20 +9,9 @@ def lcm(a,b):
         else:
             mult += 1
             continue   
-    end_time = time.time()
-    return ((end_time -start_time)*1000), larg_number 
+    return larg_number 
         
-def lcm1(a, b):
-    start_time = time.time()
-    for l in range(1, a * b + 1):
-        if l % a == 0 and l % b == 0:
-            end_time = time.time()
-            return ((end_time -start_time)*1000), l
-    assert False
-        
-
 
 if __name__ == '__main__':
     a, b = map(int, input().split())
     print(lcm(a, b))
-    print(lcm1(a, b))
--- Starter/week2_algorithmic_warmup/4_least_common_multiple/lcm.py
+++ Starter/week2_algorithmic_warmup/4_least_common_multiple/lcm.py
@@ -1,12 +1,30 @@
-def lcm(a, b):
+import time
+def lcm(a,b):
+    start_time = time.time() 
+    mult = 1
+    list = [a,b]
+    list.sort()
+    while True:       
+        larg_number = list[1] * mult
+        if larg_number % list[0] ==0:
+            break
+        else:
+            mult += 1
+            continue   
+    end_time = time.time()
+    return ((end_time -start_time)*1000), larg_number 
+        
+def lcm1(a, b):
+    start_time = time.time()
     for l in range(1, a * b + 1):
         if l % a == 0 and l % b == 0:
-            return l
-
+            end_time = time.time()
+            return ((end_time -start_time)*1000), l
     assert False
+        
 
 
 if __name__ == '__main__':
     a, b = map(int, input().split())
     print(lcm(a, b))
-
+    print(lcm1(a, b))
