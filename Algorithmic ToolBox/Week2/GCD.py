573426071c144fd64c9d9743045f15e22813e9e0
EdisonPhua <edison.phua2004@gmail.com>
2023-05-13 15:17:56 +0000

    Sat, May 13, 2023, 11:17 PM +08:00

--- Algorithmic ToolBox/Week2/GCD.py
+++ Algorithmic ToolBox/Week2/GCD.py
@@ -4,27 +4,6 @@ def gcd(a, b):
         a, b = b, a % b
     return a
 
-
-def gcd1(a, b):
-    current_gcd = 1
-    for d in range(2, min(a, b) + 1):
-        if a % d == 0 and b % d == 0:
-            if d > current_gcd:
-                current_gcd = d
-
-    return current_gcd
-
-
 if __name__ == "__main__":
     a, b = map(int, input().split())
-    
-    
-    start = time.time()
     print(gcd(a, b))
-    end = time.time()
-    print((end - start)*1000)
-    
-    start = time.time()
-    print(gcd1(a, b))
-    end = time.time()
-    print((end - start)*1000)
