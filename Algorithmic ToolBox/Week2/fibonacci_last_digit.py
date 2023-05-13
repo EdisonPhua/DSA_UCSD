08340ee40bc12e509deb0af05e98b2ae61f41b36
EdisonPhua <edison.phua2004@gmail.com>
2023-05-13 15:07:40 +0000

    Sat, May 13, 2023, 11:07 PM +08:00

--- Algorithmic ToolBox/Week2/fibonacci_last_digit.py
+++ Algorithmic ToolBox/Week2/fibonacci_last_digit.py
@@ -1,4 +1,4 @@
-
+import time
 def fibonacci_number(n):
     if n <= 1:
         return n
@@ -7,8 +7,25 @@ def fibonacci_number(n):
         for i in range(2, n+1):
             a,b = b, (b+a)
         return b%10
+    
+def calc_fib_fast(n):
+   f = []
+   f.append(0)
+   f.append(1)
+   for i in range(2,n+1):
+       f.append((f[i-1]%10+f[i-2]%10))
+   return f[-1]%10
 
 
 if __name__ == '__main__':
     input_n = int(input())
+    
+    start = time.time()
     print(fibonacci_number(input_n))
+    end = time.time()
+    print((end - start)*1000)
+
+    start = time.time()
+    print(calc_fib_fast(input_n))
+    end = time.time()
+    print((end - start)*1000)
