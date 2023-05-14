dcb113d5d9e834f826e77b4008fcc70c1df2c323
EdisonPhua <edison.phua2004@gmail.com>
2023-05-14 14:07:56 +0000

    Sun, May 14, 2023, 10:07 PM +08:00

--- Algorithmic ToolBox/Week2/fibonacci_sum_last_digit.py
+++ Algorithmic ToolBox/Week2/fibonacci_sum_last_digit.py
@@ -1,19 +1,5 @@
 
-import time
 def fibonacci_sum(n):
-    if n <= 1:
-        return n
-
-    previous, current, _sum = 0, 1, 1
-
-    for _ in range(n - 1):
-        previous, current = current, previous + current
-        _sum += current
-
-    return _sum % 10
-
-
-def fibonacci_sum1(n):
     if n <= 1:
         return n
     else:
@@ -25,18 +11,8 @@ def fibonacci_sum1(n):
 
 if __name__ == '__main__':
     n = int(input())
-    
-    
-    start = time.time()
     print(fibonacci_sum(n))
-    end = time.time()
-    print((end - start)*1000)
-    
-    
-    start = time.time()
-    print(fibonacci_sum1(n))
-    end = time.time()
-    print((end - start)*1000)
+
     
 
 
