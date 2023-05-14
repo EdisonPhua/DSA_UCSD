c46245b9fa7e56770ae06aebf849f4c09216b2ee
EdisonPhua <edison.phua2004@gmail.com>
2023-05-14 14:20:10 +0000

    Sun, May 14, 2023, 10:20 PM +08:00

--- Algorithmic ToolBox/Week2/fibonacci_sum_squares.py
+++ Algorithmic ToolBox/Week2/fibonacci_sum_squares.py
@@ -19,7 +19,7 @@ def fibonacci_sum_squares1(n):
         for i in range(n-1):
             a,b = b%10, (b+a)%10
             sum += b**2
-        return sum%10
+    return sum%10
 
 if __name__ == '__main__':
     n = int(input())
