4d7c3a44a6c4dffaf7a8ba5f5f7b2d4852015e6d
EdisonPhua <edison.phua2004@gmail.com>
2023-05-14 14:25:11 +0000

    Sun, May 14, 2023, 10:25 PM +08:00

--- Algorithmic ToolBox/Week2/fibonacci_sum_squares.py
+++ Algorithmic ToolBox/Week2/fibonacci_sum_squares.py
@@ -1,17 +1,6 @@
-import time
+
+
 def fibonacci_sum_squares(n):
-    if n <= 1:
-        return n
-
-    previous, current, sum = 0, 1, 1
-
-    for _ in range(n - 1):
-        previous, current = current, previous + current
-        sum += current ** 2
-
-    return sum % 10
-
-def fibonacci_sum_squares1(n):
     if n <= 1:
         return n
     else:
@@ -23,14 +12,4 @@ def fibonacci_sum_squares1(n):
 
 if __name__ == '__main__':
     n = int(input())
-    
-    
-    start = time.time()
     print(fibonacci_sum_squares(n))
-    end = time.time()
-    print((end - start)*1000)
-
-    start = time.time()
-    print(fibonacci_sum_squares1(n))
-    end = time.time()
-    print((end - start)*1000)
