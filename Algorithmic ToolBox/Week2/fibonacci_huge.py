e147fb23b297dc60cb1e0321877afa0622c8a443
EdisonPhua <edison.phua2004@gmail.com>
2023-05-14 14:32:09 +0000

    Sun, May 14, 2023, 10:32 PM +08:00

--- Algorithmic ToolBox/Week2/fibonacci_huge.py
+++ Algorithmic ToolBox/Week2/fibonacci_huge.py
@@ -1,15 +1,6 @@
-import time
+
+
 def fibonacci_huge_naive(n,m):
-    if n <= 1:
-        return n
-    else:
-        a,b = 0,1
-        for i in range(2, n+1):
-            a,b = b%m, (b+a)%m
-        return b%m
-
-
-def fibonacci_huge_naive1(n,m):
     if n <= 1:
         return n
     else:
@@ -20,14 +11,4 @@ def fibonacci_huge_naive1(n,m):
 
 if __name__ == '__main__':
     n, m = map(int, input().split())
-    
-    
-    
-    start = time.time()
     print(fibonacci_huge_naive(n, m))
-    end = time.time()
-    print((end - start)*1000)
-
-    start = time.time()
-    print(fibonacci_huge_naive1(n, m))
-    print((end - start)*1000)
