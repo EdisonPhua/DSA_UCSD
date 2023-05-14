4e281287335d90cc6151b8d09ea88442cdd36865
EdisonPhua <edison.phua2004@gmail.com>
2023-05-14 14:18:08 +0000

    Sun, May 14, 2023, 10:18 PM +08:00

--- Algorithmic ToolBox/Week2/fibonacci_partial_sum.py
+++ Algorithmic ToolBox/Week2/fibonacci_partial_sum.py
@@ -1,22 +1,7 @@
 # Uses python3
-import sys, time
+import sys
 
 def fibonacci_partial_sum_naive(from_, to):
-    _sum = 0
-
-    current = 0
-    _next  = 1
-
-    for i in range(to + 1):
-        if i >= from_:
-            _sum += current
-
-        current, _next = _next, current + _next
-
-    return _sum % 10
-
-
-def fibonacci_partial_sum_naive1(from_, to):
     sum , current, next = 0,0,1
     
     for i in range(to + 1):
@@ -30,15 +15,3 @@ if __name__ == '__main__':
     input = sys.stdin.read();
     from_, to = map(int, input.split())
     print(fibonacci_partial_sum_naive(from_, to))
-
-
-    start = time.time()
-    print(fibonacci_partial_sum_naive(from_, to))
-    end = time.time()
-    print((end - start)*1000)
-    
-    start = time.time()
-    print(fibonacci_partial_sum_naive1(from_, to))
-    end = time.time()
-    print((end - start)*1000)
-    
--- Algorithmic ToolBox/Week2/fibonacci_sum_squares.py
+++ Algorithmic ToolBox/Week2/fibonacci_sum_squares.py
@@ -1,3 +1,4 @@
+import time
 def fibonacci_sum_squares(n):
     if n <= 1:
         return n
@@ -10,7 +11,26 @@ def fibonacci_sum_squares(n):
 
     return sum % 10
 
+def fibonacci_sum_squares1(n):
+    if n <= 1:
+        return n
+    else:
+        a,b, sum = 0,1,1
+        for i in range(n-1):
+            a,b = b%10, (b+a)%10
+            sum += b**2
+        return sum%10
 
 if __name__ == '__main__':
     n = int(input())
+    
+    
+    start = time.time()
     print(fibonacci_sum_squares(n))
+    end = time.time()
+    print((end - start)*1000)
+
+    start = time.time()
+    print(fibonacci_sum_squares1(n))
+    end = time.time()
+    print((end - start)*1000)
