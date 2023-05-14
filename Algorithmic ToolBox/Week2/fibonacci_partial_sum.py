ebaf32cebb2ec42134306a59bf2f39b15268f888
EdisonPhua <edison.phua2004@gmail.com>
2023-05-14 14:10:46 +0000

    Sun, May 14, 2023, 10:10 PM +08:00

--- Algorithmic ToolBox/Week2/fibonacci_partial_sum.py
+++ Algorithmic ToolBox/Week2/fibonacci_partial_sum.py
@@ -16,6 +16,19 @@ def fibonacci_partial_sum_naive(from_, to):
     return _sum % 10
 
 
+def fibonacci_partial_sum_naive1(from_, to):
+    sum , current, next = 0,0,1
+    
+    for i in range(to + 1):
+        
+        
+        
+        
+        a,b = b%10, (b+a)%10
+        sum += b
+    return sum%10
+
+
 if __name__ == '__main__':
     input = sys.stdin.read();
     from_, to = map(int, input.split())
