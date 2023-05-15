aa34345a8b427329b382408e87ae23e92ce76a8f
EdisonPhua <edison.phua2004@gmail.com>
2023-05-15 10:44:45 +0000

    Mon, May 15, 2023, 6:44 PM +08:00

--- Algorithmic ToolBox/Week3/3-7.py
+++ Algorithmic ToolBox/Week3/3-7.py
@@ -1,26 +1,28 @@
-from itertools import permutations
-def IsBetter(a,b):
-    a,b = str(a) , str(b)
-    
-    if int((a+b)) > int(b+a):
-        return True
-    else:
-        return False
 
-def largest_number_naive(numbers):
-    numbers = list(map(str, numbers))
 
-    largest = []
+import sys
 
-    for permutation in permutations(numbers):
-        
-        if IsBetter("".join(permutation)):
-            largest.append(permutation)
+def largest_number(a,n):
+    #write your code here
+    res = ""
+    while(a):
+        maxi=a[0]
+        maxindex=0
+        for i,each in enumerate(a):
+            if IsGreaterOrEqual_v2(each,maxi):
+                maxi=each
+                maxindex=i
+        res += maxi
+        del a[maxindex]
+    return res
 
-    return largest
-
+def IsGreaterOrEqual_v2(A,B):
+    return int(A+B)>int(B+A)
 
 if __name__ == '__main__':
-    _ = int(input())
-    input_numbers = input().split()
-    print(largest_number_naive(input_numbers))
+    input = sys.stdin.read()
+    data = input.split()
+    n=int(data[0])
+    a = data[1:]
+    print(largest_number(a,n))
+    
