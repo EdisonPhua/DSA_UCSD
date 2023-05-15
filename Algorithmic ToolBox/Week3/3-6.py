3a59d1bd94ff802794c81b6d9538b85b37878eb7
EdisonPhua <edison.phua2004@gmail.com>
2023-05-15 11:09:48 +0000

    Mon, May 15, 2023, 7:09 PM +08:00

--- Algorithmic ToolBox/Week3/3-6.py
+++ Algorithmic ToolBox/Week3/3-6.py
@@ -1,13 +1,15 @@
 def optimal_summands(n):
     summands = []
     # write your code here
-    current_numer = 1
-    while n >= (2 * current_numer):
-        summands.append(current_numer)
-        
-        n -= current_numer
-        current_numer += 1   
-    summands.append(n) 
+    current_summand = 1
+    while n > 0:
+        if n <= 2 * current_summand:
+            summands.append(n)
+            break
+
+        summands.append(current_summand)
+        n -= current_summand
+        current_summand += 1
         
         
     return summands
