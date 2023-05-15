5e3815f2e356ebbd669c7408b983e0ff76fc7517
EdisonPhua <edison.phua2004@gmail.com>
2023-05-15 07:14:15 +0000

    Mon, May 15, 2023, 3:14 PM +08:00

--- Algorithmic ToolBox/Week3/3-2.py
+++ Algorithmic ToolBox/Week3/3-2.py
@@ -8,7 +8,13 @@ def optimal_value(capacity, weights, values):
     n = len(weights)
     for i in range(n):
         val.append((values[i]/weights[i], weights[i]))
-    val.sort(reverse=True) #index 0 will be the highest cost
+    val.sort(reverse=True) #index 0 will be the highest cost firstD is cost, secondD is weights
+    
+    i = 0
+    while capacity>0 and i<n:
+        if capacity> val[i][1]:
+            value += val[i][0]*val[i][1]
+            capacity -= val[i][1]
 
 
 if __name__ == "__main__":
