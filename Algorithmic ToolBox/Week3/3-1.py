794ae3a07c413dc7265d90fa725623e487c21c6b
EdisonPhua <edison.phua2004@gmail.com>
2023-05-15 06:27:12 +0000

    Mon, May 15, 2023, 2:27 PM +08:00

--- Algorithmic ToolBox/Week3/3-1.py
+++ Algorithmic ToolBox/Week3/3-1.py
@@ -1,7 +1,22 @@
 def change(money):
     # write your code here
+    numCoins = 0
     
-    return money
+    while True:
+        if money >= 10:
+            money -= 10
+            numCoins += 1
+            continue
+        elif money >=5:
+            money -= 5
+            numCoins += 1
+            continue
+        elif money >= 1:
+            money -= 1
+            numCoins +=1
+            break
+        
+    return numCoins
 
 
 if __name__ == '__main__':
