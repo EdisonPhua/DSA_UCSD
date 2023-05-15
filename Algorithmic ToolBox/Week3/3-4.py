9d083f436b03a74358e31a66e0cb01e3b05b477c
EdisonPhua <edison.phua2004@gmail.com>
2023-05-15 08:31:03 +0000

    Mon, May 15, 2023, 4:31 PM +08:00

--- Algorithmic ToolBox/Week3/3-4.py
+++ Algorithmic ToolBox/Week3/3-4.py
@@ -3,6 +3,8 @@ from itertools import permutations
 
 def max_dot_product(first_sequence, second_sequence):
     max_product = 0
+    first_sequence.sort()
+    second_sequence.sort()
     for permutation in permutations(second_sequence):
         dot_product = sum(first_sequence[i] * permutation[i] for i in range(len(first_sequence)))
         max_product = max(max_product, dot_product)
