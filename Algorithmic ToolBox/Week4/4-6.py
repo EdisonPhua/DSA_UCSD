83a7c7a94cb777c46a49028e108ed2bac58a0a0b
EdisonPhua <edison.phua2004@gmail.com>
2023-05-17 13:02:27 +0000

    Wed, May 17, 2023, 9:02 PM +08:00

--- Algorithmic ToolBox/Week4/4-6.py
+++ Algorithmic ToolBox/Week4/4-6.py
@@ -1,14 +1,31 @@
 from sys import stdin
 
 
-def points_cover_naive(starts, ends, points):
+def points_cover(starts, ends, points):
     assert len(starts) == len(ends)
     count = [0] * len(points)
 
-    for index, point in enumerate(points):
-        for start, end in zip(starts, ends):
-            if start <= point <= end:
-                count[index] += 1
+    # Combine start and end points into a single list with their type
+    events = [(start, 'start') for start in starts] + [(end, 'end') for end in ends]
+
+    # Sort the combined list
+    events.sort()
+
+    # Counter to track the number of active segments
+    active_segments = 0
+
+    # Iterate over the sorted list
+    for event, event_type in events:
+        if event_type == 'start':
+            active_segments += 1
+        else:  # event_type == 'end'
+            active_segments -= 1
+
+        # Find the index of the point in the original points list
+        point_index = points.index(event)
+
+        # Update the count for the point
+        count[point_index] = active_segments
 
     return count
 
@@ -21,5 +38,5 @@ if __name__ == '__main__':
     input_starts, input_ends = data[2:2 * n + 2:2], data[3:2 * n + 2:2]
     input_points = data[2 * n + 2:]
 
-    output_count = points_cover_naive(input_starts, input_ends, input_points)
+    output_count = points_cover(input_starts, input_ends, input_points)
     print(*output_count)
