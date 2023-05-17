296de11751513bd5ece87241c397c85d28c1e5bd
EdisonPhua <edison.phua2004@gmail.com>
2023-05-17 05:12:28 +0000

    Wed, May 17, 2023, 1:12 PM +08:00

--- Algorithmic ToolBox/Week4/4-2.py
+++ Algorithmic ToolBox/Week4/4-2.py
@@ -9,7 +9,7 @@ def binary_search(keys, query):
         mid =  (minIndex +maxIndex)//2
         if keys[mid] == query:
             last_occurance = mid
-            minIndex = mid + 1           
+            maxIndex = mid - 1            
         elif keys[mid] <  query:
             minIndex = mid + 1
         elif keys[mid] > query:
